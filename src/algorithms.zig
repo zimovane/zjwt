4116faac123a0f947e7f607253628da2c7a12685
Amovane <amovane@163.com>
2023-05-04 02:29:53 +0800

    minor fixes

--- src/algorithms.zig
+++ src/algorithms.zig
@@ -13,27 +13,28 @@ pub const HMACAlgorithm = enum {
     HS384,
     HS512,
 
-    fn sha2(self: Self) HMAC.Hmac {
-        switch (self) {
+    fn sha2(comptime self: Self) type {
+        return switch (self) {
             .HS256 => HMAC.sha2.HmacSha256,
             .HS384 => HMAC.sha2.HmacSha384,
             .HS512 => HMAC.sha2.HmacSha512,
-        }
+        };
     }
 
-    pub fn verify(self: Self, sig_bytes: []u8, msg: []const u8, key: []u8) !void {
+    pub fn verify(comptime self: Self, sig_bytes: []const u8, msg: []const u8, key: []const u8) Err.SignatureVerificationError!void {
         const sig = self.sign(msg, key);
-        if (!utils.compareSlices(u8, sig_bytes, sig)) {
-            return Err.SignatureVerificationError;
+        if (utils.compareSlices(u8, sig_bytes, &sig)) {
+            return Err.SignatureVerificationError.SignatureVerificationFailed;
         }
+        return;
     }
 
-    pub fn sign(self: Self, msg: []const u8, key: []const u8) [self.sha2().mac_length]u8 {
+    pub fn sign(comptime self: Self, msg: []const u8, key: []const u8) [self.sha2().mac_length]u8 {
         const alg = self.sha2();
         var out: [alg.mac_length]u8 = undefined;
-        const hmac = alg.init(key);
+        var hmac = alg.init(key);
         hmac.update(msg);
-        hmac.final(out);
+        hmac.final(&out);
         return out;
     }
 };
@@ -43,20 +44,20 @@ pub const EcdsaAlgorithm = enum {
     ES256,
     ES384,
 
-    fn ecdsa(self: Self) Ecdsa {
-        switch (self) {
+    fn ecdsa(comptime self: Self) type {
+        return switch (self) {
             .ES256 => Ecdsa.EcdsaP256Sha256,
             .ES384 => Ecdsa.EcdsaP384Sha384,
-        }
+        };
     }
 
-    pub fn verify(self: Self, sig_bytes: []u8, msg: []const u8, pbkey_bytes: []u8) (Err.IdentityElementError || Err.NonCanonicalError || Err.SignatureVerificationError)!void {
+    pub fn verify(comptime self: Self, sig_bytes: []const u8, msg: []const u8, pbkey_bytes: []const u8) (Err.IdentityElementError || Err.NonCanonicalError || Err.SignatureVerificationError)!void {
         const alg = self.ecdsa();
         const sig = alg.Signature.fromBytes(sig_bytes);
         const pbkey = alg.PublicKey.fromSec1(pbkey_bytes);
         const verifier = try sig.verifier(pbkey);
         verifier.update(msg);
-        verifier.verify();
+        return verifier.verify();
     }
 };
 
@@ -65,7 +66,7 @@ pub const RSAAlgorithm = enum {
     RS256,
     RS384,
     RS512,
-    pub fn verify(_: Self, sig_bytes: [64]u8, msg: []const u8, pbkey_bytes: [64]u8) void {
+    pub fn verify(_: Self, sig_bytes: []u8, msg: []const u8, pbkey_bytes: []u8) void {
         _ = pbkey_bytes;
         _ = msg;
         _ = sig_bytes;
@@ -75,11 +76,11 @@ pub const RSAAlgorithm = enum {
 pub const Ed25519Algorithm = enum {
     const Self = @This();
     EdDSA,
-    pub fn verify(_: Self, sig_bytes: [64]u8, msg: []const u8, pbkey_bytes: [64]u8) (Err.SignatureVerificationError || Err.IdentityElementError || Err.WeakPublicKeyError || Err.EncodingError || Err.NonCanonicalError)!void {
+    pub fn verify(comptime _: Self, sig_bytes: []const u8, msg: []const u8, pbkey_bytes: []const u8) (Err.SignatureVerificationError || Err.IdentityElementError || Err.WeakPublicKeyError || Err.EncodingError || Err.NonCanonicalError)!void {
         const sig = Ed25519.Signature.fromBytes(sig_bytes);
         const pbkey = try Ed25519.Publickey.fromBytes(pbkey_bytes);
         const verifier = try sig.verifier(pbkey);
         verifier.update(msg);
-        verifier.verify();
+        return verifier.verify();
     }
 };
